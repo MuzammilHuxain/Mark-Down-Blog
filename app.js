e038a289f2f839ba86ac2ebf2a1e37c367b1dcb0
MuzammilHuxain <muzammilch2021@gmail.com>
2025-05-11 19:58:51 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const Article = require("./models/article");
+const articleRouter = require("./routes/articles");
+const methodOverride = require("method-override");
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((err) => {
+    console.log("Error connecting to database", err);
+  });
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: "desc" });
+  res.render("articles/index", { articles: articles });
+});
+
+app.use("/articles", articleRouter);
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server is running on port ${PORT}`);
+});
