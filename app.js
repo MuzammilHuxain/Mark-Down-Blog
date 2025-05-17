bd8083e9c341bd88c6dd7c266b229197266adff3
MuzammilHuxain <muzammilch2021@gmail.com>
2025-05-17 21:14:07 +0500

    Updating files

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const methodOverride = require("method-override");
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
